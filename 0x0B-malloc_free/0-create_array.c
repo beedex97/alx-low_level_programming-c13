2d9ba68ef556bc1f287ab5e1256839746f1b83c2
beedex97 <abdulraheemyubidemi@gmail.com>
2023-04-11 21:55:38 -0700

    array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,25 +1,23 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
+ * create_array - create array of size size and assign char c
+ * @size: size of array
+ * @c: char to assign
+ * Description: create array of size size and assign char c
+ * Return: pointer to array, NULL if fail
  *
- * Return: Nothing.
  */
 char *create_array(unsigned int size, char c)
 {
-  unsigned int i;
+	char *str;
+	unsigned int i;
 
-  if (size == 0)
-      return (NULL);
+	str = malloc(sizeof(char) * size);
+	if (size == 0 || str == NULL)
+		return (NULL);
 
-  char *s = (char*)malloc(size * sizeof(char));
-
-  for (i = 0; i < size; i++)
-    s[i] = c;
-
-  return (s);
+	for (i = 0; i < size; i++)
+		str[i] = c;
+	return (str);
 }
